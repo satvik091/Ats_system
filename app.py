6104c0facbf9964068fb6735cd4eeb6782cc3104
SATVIK GUPTA <satvikg21111124@gmail.com>
2024-12-26 21:22:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 # Configure Google Generative AI
-genai.configure(api_key=("AIzaSyACVj30Yv9r03qxBz-b7oKUf_JUHMHbKTw"))
+genai.configure(api_key=("AIzaSyBGKcyTyznlGzbPuivbcnU3fVgJVrUl_24"))
 
 def get_gemini_response(resume_text, job_desc_text, prompt):
     """Fetches a response from Gemini API."""
